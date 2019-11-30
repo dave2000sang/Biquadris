5640feab392392e069ec904a3b2ac75b1647f469
dave2000sang <david2000sang@hotmail.com>
2019-11-30 00:35:32 -0500

    support throwing game over

--- game_state.cc
+++ game_state.cc
@@ -4,51 +4,61 @@ using namespace std;
 GameState::GameState() : p1{make_unique<Player>()}, p2{make_unique<Player>()}, turn{0} {}
 
 void GameState::rotate(int reps, string dir) {
-    turn % 2 == 0 ? p1.rotate(reps, dir) : p2.rotate(reps, dir);
+    for (int i = 0; i < reps; i++) {
+        turn % 2 == 0 ? p1->rotate(dir) : p2->rotate(dir);
+    }
     turn++;
 }
 
 void GameState::translate(int reps, int x, int y) {
-    turn % 2 == 0 ? p1.translate(reps, x, y) : p2.translate(reps, x, y);
+    for (int i = 0; i < reps; i++) {
+        turn % 2 == 0 ? p1->translate(x, y) : p2->translate(x, y);
+    }
     turn++;
 }
 
-void GameState::drop() {
-    turn % 2 == 0 ? p1.drop() : p2.drop();
+void GameState::drop(int multiplier) {
+    for (int i = 0; i < multiplier; i++) {
+        try {
+            turn % 2 == 0 ? p1->drop() : p2->drop();
+        } catch (string s) {
+            // game over for current player.
+            if (turn % 2 == 0) {
+                throw 2;
+            } else {
+                throw 1;
+            }
+        }
+    }
 }
 
 void GameState::levelUp(int multiplier) {
-    turn % 2 == 0 ? p1.levelUp(multiplier) : p2.levelUp(multiplier);
+    for (int i = 0; i < multiplier; i++) {
+        turn % 2 == 0 ? p1->levelUp() : p2->levelUp();
+    }
     turn++;
 }
 
 void GameState::levelDown(int multiplier) {
-    turn % 2 == 0 ? p1.levelDown(multiplier) : p2.levelDown(multiplier);
+    for (int i = 0; i < multiplier; i++) {
+        turn % 2 == 0 ? p1->levelDown() : p2->levelDown();
+    }
     turn++;
 }
 
 void GameState::noRandom(std::string file) {
-    turn % 2 == 0 ? p1.noRandom(file) : p2.noRandom(file);
+    turn % 2 == 0 ? p1->noRandom(file) : p2->noRandom(file);
     turn++;
 }
 
 void GameState::setRandom() {
-    turn % 2 == 0 ? p1.setRandom() : p2.setRandom();
+    turn % 2 == 0 ? p1->setRandom() : p2->setRandom();
     turn++;
 }
 
 void GameState::replaceBlock(char block) {
-    turn % 2 == 0 ? p1.replaceBlock(block) : p2.replaceBlock(block);
+    turn % 2 == 0 ? p1->replaceBlock(block) : p2->replaceBlock(block);
     turn++;
-}
-
-// restarts entire game
-void GameState::restart() {
-    turn = 0;
-    p1.reset();
-    p2.reset();
-    p1 = make_unique<Player>();
-    p2 = make_unique<Player>();
 }
 
 void GameState::updateTurn() {
